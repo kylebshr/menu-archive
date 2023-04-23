a8a488e0971ef858db28517a7ff91951128ff94e
Kyle Bashour <kylebshr@me.com>
2023-04-23 10:55:13 -0700

    Add menu

---
+++ MenuMenu/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MenuMenu
+//
+//  Created by Kyle Bashour on 4/23/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
